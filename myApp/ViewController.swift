92e130aae972e8068aff530c16e784f74c34678e
SeongwooKang <cyrus2860@gmail.com>
2022-06-21 16:48:23 +0900

    Initial Commit

---
+++ myApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myApp
+//
+//  Created by Kang Seongwoo on 2022/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
